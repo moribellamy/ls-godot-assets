915f9b6813f6701c2799255f17eda7ea7830e109
Mori Bellamy <mori@invoked.net>
2023-12-01 20:31:06 -0800

    iter

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,139 @@
 import {Octokit} from "octokit";
-import {default as axios} from "axios";
-import {myflag} from "./flags";
+import {writeFile} from "fs";
+import {AxiosResponse, default as axios} from "axios";
+import {MultiBar, SingleBar, Presets} from "cli-progress";
+import {number, boolean, command, flag, option, run, string} from "cmd-ts";
+import {AssetResult, PaginatedGodotAssets, parse_github, ParsedGithub} from "./godot_assets";
 
-// https://godotengine.org/asset-library/api/asset?godot_version=any&type=any&max_results=1
-async function main() {
-    const octokit = new Octokit();
-    const repo = await octokit.rest.repos.get(
-        {
-            owner: "moribellamy",
-            repo: "porygon"
-        }
-    );
-    console.log(repo.data.stargazers_count);
 
-    const asset_search_response = await axios.get("https://godotengine.org/asset-library/api/asset", {
-        params: {
-            godot_version: "any",
-            type: "any",
-            max_results: "1"
-        }
-    });
-
-    console.log(asset_search_response.data);
-
-    // const asset_id = asset_search_response.data.result["id"];
-
-    // const asset = await axios.get(`https://godotengine.org/asset-library/api/asset/${asset_id}`);
-    const asset = await axios.get("https://godotengine.org/asset-library/api/asset/2089");
-
-    console.log(asset.data);
-
-    console.log(myflag);
+type GithubRepoSummary = {
+    repo_url: string,
+    stargazers: number
 }
 
-main();
+async function repo_summary(parsed_github: ParsedGithub, octokit: Octokit): Promise<GithubRepoSummary> {
+    const repo_info = await octokit.rest.repos.get(
+        {
+            owner: parsed_github.author,
+            repo: parsed_github.repo
+        }
+    );
+    return {
+        repo_url: `https://github.com/${parsed_github.author}/${parsed_github.repo}`,
+        stargazers: repo_info.data.stargazers_count
+    };
+}
+
+const app = command({
+    name: "ls-godot-assets",
+    args: {
+        check_rate: flag({
+            type: boolean,
+            long: "check_rate",
+            short: "r",
+            description: "Check the remaining rate limit for the given <gh_api_key>."
+        }),
+        out: option({
+            type: string,
+            long: "out",
+            short: "o",
+            defaultValue: () => "",
+            description: "If given, write the output to the given file as JSON."
+        }),
+        gh_api_key: option({
+            type: string,
+            long: "github_api_key",
+            short: "k",
+            defaultValue: () => "",
+            description: "If given, use as the github API key for star-getting requests."
+        }),
+        limit: option({
+            type: number,
+            long: "limit",
+            short: "l",
+            defaultValue: () => -1,
+            description: "Stop scraping projects after this many. <0 means infinity."
+        }),
+        page_size: option({
+            type: number,
+            long: "page_size",
+            short: "p",
+            defaultValue: () => 200,
+            description: "How many godot assets to GET in one batch."
+        })
+    },
+    handler: async ({
+                        out,
+                        check_rate,
+                        gh_api_key,
+                        limit,
+                        page_size
+                    }) => {
+        const octokit = new Octokit({auth: gh_api_key});
+
+        if (check_rate) {
+            const rate_limit = await octokit.rest.rateLimit.get();
+            const rate = rate_limit.data.rate;
+            const resetDate = new Date(rate.reset * 1000);
+            const resetDateString = resetDate.toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: true,
+            });
+            console.log(`${rate.remaining}/${rate.limit} core requests remaining (reset ${resetDateString})`);
+            return;
+        }
+
+        // Godot Phase
+        const multibar = new MultiBar({format: "{bar} | {value}/{total} | {name}"}, Presets.shades_grey);
+        const paginated = new PaginatedGodotAssets(page_size);
+        const parsed_githubs: ParsedGithub[] = [];
+        let godot_bar;
+
+        for (let asset_idx = 0; limit < 0 || asset_idx < limit; asset_idx++) {
+            const asset = await paginated.next();
+            if (asset == null) {
+                break;
+            }
+            if (asset_idx == 0) {
+                godot_bar = multibar.create(limit < 0 ? paginated.get_total() : limit, 0);
+                godot_bar.update(0, {name: "godot"});
+            }
+            godot_bar.increment();
+            const parsed_github = await parse_github(asset);
+            if (parsed_github == null) {
+                continue;
+            }
+            parsed_githubs.push(parsed_github);
+        }
+
+        // Github Phase
+        const github_repo_promises: Promise<GithubRepoSummary>[] = [];
+        const github_bar = multibar.create(parsed_githubs.length, 0);
+        github_bar.update(0, {name: "github"});
+
+        for (const parsed_github of parsed_githubs) {
+            github_repo_promises.push(repo_summary(parsed_github, octokit).then(summ => {
+                github_bar.increment();
+                return summ;
+            }));
+        }
+
+        const resolved = await Promise.all(github_repo_promises);
+        multibar.stop();
+
+        // Finish Phase
+        if (out == "") {
+            console.log(resolved);
+        } else {
+            writeFile(out, JSON.stringify(resolved), err => {
+                if (err) {
+                    console.error("Cannot write file", err);
+                }
+            });
+        }
+    },  // main handler
+});  // app def
+
+
+run(app, process.argv.slice(2));
--- src/flags.ts
+++
@@ -1,2 +0,0 @@
-
-export const myflag = "hello";
